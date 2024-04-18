59c598c89976488047d1442ddb116a3ec5cb5e6a
majo118 <mj.bermudez2@uniandes.edu.co>
2024-04-17 22:36:04 -0500

    prueba colisiones 289238139823918

--- src/Galeria/Galeria.java
+++ src/Galeria/Galeria.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class Galeria {
 	
-	private Map<String,Pieza> Inventario= new HashMap<String,Pieza>();
+	private Map<String,Pieza> pedro= new HashMap<String,Pieza>();
 	private Map<String,Pieza> Subasta= new HashMap<String,Pieza>();
 	private Map<String,Usuario> Usuarios= new HashMap<String,Usuario>();
 	/*Subasta sera mas pequeña que Inventario porque solo estaran los elementos que tengan subasta en True*/
@@ -30,6 +30,6 @@ public class Galeria {
 	
 	public void AgregarUsuario(Usuario usuario) {
 		/*Agrega un usuario al hash map de Usuario*/
-		/*Prueba 2*/
+		/*x*/
 	}
 }
